5724abc8ca04a74407422cd20f44971d8640478e
jikim127 <jikim127@gmail.com>
2023-06-22 15:34:09 +0900

    jimin : insertMember+selectOneProductByNo 합침

--- src/main/java/com/example/ourproject/dao/DAO.java
+++ src/main/java/com/example/ourproject/dao/DAO.java
@@ -1,6 +1,11 @@
 package com.example.ourproject.dao;
 
+import com.example.ourproject.VO.MemberVO;
 import com.example.ourproject.VO.ProductVO;
+import com.example.ourproject.util.DBManager;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 public class DAO {
 
@@ -13,6 +18,40 @@ public class DAO {
         return instance;
     }
 
+
+    public int insertMember(MemberVO vo) {
+
+        int result = -1;
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        String sql = "insert into member values(?,?,?,?,?,?,?)";
+
+        try{
+            conn = DBManager.getConnection();
+            pstmt = conn.prepareStatement(sql);
+
+            pstmt.setString(1, vo.getId());
+            pstmt.setString(2, vo.getPw());
+            pstmt.setString(3, vo.getEmail());
+            pstmt.setString(4, vo.getName());
+            pstmt.setString(5, vo.getBirthDate());
+            pstmt.setString(6, vo.getGender());
+            pstmt.setString(7, vo.getPhone());
+
+            result = pstmt.executeUpdate();
+
+        }catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            DBManager.close(conn,pstmt);
+        }
+        return result;
+    }
+
+
     public ProductVO selectOneProductByNo(int no) {
     }
 }
+
