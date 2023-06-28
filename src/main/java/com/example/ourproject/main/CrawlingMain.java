f3c9f03924b7811b5d7cf715495fbd76fae1217e
박종진 <sam990629@naver.com>
2023-06-28 19:44:32 +0900

    CrawlingMain Update

--- src/main/java/com/example/ourproject/main/CrawlingMain.java
+++ src/main/java/com/example/ourproject/main/CrawlingMain.java
@@ -19,40 +19,38 @@ public class CrawlingMain {
 
         try {
             int k = 1;
-            for(int i = 1; i<=1; i++){
-                Document doc = Jsoup.connect("https://front.wemakeprice.com/main").get();
-                Elements poster = doc.select("div.container_inner div.contents_wrap");
-                System.out.println(doc);
-//                String imageUrl = null;
-//
-//                for (Element imgElement : poster) {
-//                    //image url 가져오기
-//                    imageUrl = imgElement.attr("src");
-//                    imageUrl = imageUrl.replace("data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==","");
-//
-//                    //title 가져오기
-//                    String title = imgElement.attr("alt");
-//                    String[] titleParts = title.split(" - ");
-//                    title = titleParts[0];
-//
-//                    ProductVO vo = new ProductVO();
-//
-//                    vo.setNo(k++);
-//                    vo.setImage(imageUrl);
-//                    vo.setTitle(title);
-//                    vo.setPrice((int)(Math.random()*7000)+7000);
-//
-//                    dao.productInsert(vo);
-//                }
-//
-//
+            for(int i = 1; i<=16; i++){
+                Document doc = Jsoup.connect("https://www.gmarket.co.kr/n/smilefresh/category?categoryCode=100001003&p="+i).get();
+                Elements poster = doc.select("div.box__itemcard div.box__items-list ul.list__itemcard-slide li.list-item div.box__itemcard-container div.box__itemcard-body div.box__itemcard-inner a.link__itemcard span.box__itemcard-img img");
+                System.out.println(poster);
+                String imageUrl = null;
 
 
+                for (Element imgElement : poster) {
+                    //image url 가져오기
+                    imageUrl = imgElement.attr("src");
+                    imageUrl = imageUrl.replace("data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==","");
+                    System.out.println(imageUrl);
+
+                    //title 가져오기
+                    String title = imgElement.attr("alt");
+                    String[] titleParts = title.split(" - ");
+                    title = titleParts[0];
+
+                    System.out.println(title);
+
+                    ProductVO vo = new ProductVO();
+
+                    vo.setNo(k++);
+                    vo.setImage(imageUrl);
+                    vo.setTitle(title);
+                    vo.setPrice((int)(Math.random()*7000)+7000);
+
+                    dao.productInsert(vo);
+                }
             }
-
         }catch (Exception e){
             e.printStackTrace();
         }
-
     }
 }
