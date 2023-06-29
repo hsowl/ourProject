abdefa949f2d774f1f7d37bbceeee09ec43eee1a
leehal <annaleehalim@gmail.com>
2023-06-29 09:28:51 +0900

    끝!!

--- src/main/webapp/script/CheckBox.js
+++
@@ -1,7 +0,0 @@
-function selectAll(selectAll){
-    const  checkboxes = document.getElementsByName('no');
-
-    checkboxes.forEach((checkbox)=>{
-        checkbox.checked = selectAll.checked;
-})
-}
